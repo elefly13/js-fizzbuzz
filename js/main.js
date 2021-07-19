8b6f95182cb3aa894513d355cd551578ba3eb6ee
Elefly13 <elymara13@gmail.com>
2021-07-19 15:09:49 +0200

    primo step

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 //Scrivi un programma che stampi i numeri da 1 a 100,
+var insieme_numeri = 100;
 
+for (var i = 0; i < insieme_numeri; i++) {
+    console.log(i + 1);
+}
 
 //ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+
 
 
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.//
